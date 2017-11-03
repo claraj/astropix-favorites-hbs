d4967b9c7fb88fa739dc28ffc03be010c3731978
clara <clara@clara.com>
2017-11-02 19:38:19 -0500

    sessions for astropix

---
+++ README.md
@@ -0,0 +1,11 @@
+## AstroPix
+
+Space is really, really cool.
+
+Website to fetch today's Astronomy Picture of the Day from NASA, or a random picture.
+
+Uses https://api.nasa.gov/api.html.
+
+You need a APOD key, create by signing up at the link above.
+
+Set an environment variable `APOD_API_KEY` with value of your API key.
---
+++ app.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var hbs = require('hbs');
+var session = require('express-session');
+var MongoDBStore = require('connect-mongodb-session')(session);
+
+var index = require('./routes/index');
+var favorites = require('./routes/favorites');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var mongo_url = process.env.MONGO_URL;
+
+// Configure session store. Remember to configure DB url for this app's database
+var store = new MongoDBStore({ uri: mongo_url, collection: 'sessions'}, function(err) {
+  if (err) {
+  console.log("Error, can't connect to MongoDB to store sessions");
+  }
+});
+
+// Configure sessions
+app.use(session({
+  secret: 'top secret!',
+  resave: true,
+  saveUninitialized: true,
+  store: store
+}));
+
+
+app.use('/', index);
+app.use('/favorites', favorites);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
